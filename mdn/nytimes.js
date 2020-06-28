2f930210f646bebd64dde2f142a919282545f933
Anthony Kuang <kuanganthony@gmail.com>
2020-06-27 19:36:17 -0700

    nytimes

--- mdn/nytimes.js
+++ mdn/nytimes.js
@@ -2,15 +2,10 @@ const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 const key = 'eDlx7oL67iYdUgGksvZGwusNOihEGsEX';
 let url;
 
-
-
 const searchTerm = document.querySelector('.search');
 const startDate = document.querySelector('.start-date');
 const endDate = document.querySelector('.end-date');
 const searchForm = document.querySelector('form');
-
-// This is never used
-// const submitBtn = document.querySelector('.submit');
 
 const nextBtn = document.querySelector('.next');
 const previousBtn = document.querySelector('.prev');
@@ -18,14 +13,11 @@ const previousBtn = document.querySelector('.prev');
 const section = document.querySelector('section');
 const nav = document.querySelector('nav');
 
-// Hide the "Previous"/"Next" navigation to begin with, as we don't need it immediately
 nav.style.display = 'none';
 
-// define the initial page number and status of the navigation being displayed
 let pageNumber = 0;
 
-// This is never used
-// let displayNav = false;
+
 
 function submitSearch(e) {
   pageNumber = 0;
@@ -35,40 +27,43 @@ function submitSearch(e) {
 function fetchResults(e) {
   e.preventDefault();
 
-  url = `${baseURL}?api-key${key}&page=${pageNumber}&q=${searchTerm.value}&fq=document_type:("article)`;
+  url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")';
 
-  if (startDate.value !== '') {
-    url += `&begin_date=${startDate.value}`;
+  if(startDate.value !== '') {
+    url += '&begin_date=' + startDate.value;
   };
 
-  if (endDate.value !== '') {
-    url += `&end_date=${endDate.value}`;
+  if(endDate.value !== '') {
+    url += '&end_date=' + endDate.value;
   };
 
-  fetch(url)
-    .then(result => result.json())
-    .then(json => displayResults(json));
+  fetch(url).then(function(result) {
+    return result.json();
+  }).then(function(json) {
+    displayResults(json);
+  });
 }
 
 function displayResults(json) {
   while (section.firstChild) {
-    section.removeChild(section.firstChild);
+      section.removeChild(section.firstChild);
   }
 
   const articles = json.response.docs;
+  console.log(articles);
 
-  if (articles.length === 10) {
+  if(articles.length === 10) {
     nav.style.display = 'block';
   } else {
     nav.style.display = 'none';
   }
 
-  if (articles.length === 0) {
+  if(articles.length === 0) {
     const para = document.createElement('p');
-    para.textContent = 'No results returned.';
+    para.textContent = 'No results returned.'
     section.appendChild(para);
   } else {
-    for (var i = 0; i < articles.length; i++) {
+    for(let i = 0; i < articles.length; i++) {
       const article = document.createElement('article');
       const heading = document.createElement('h2');
       const link = document.createElement('a');
@@ -84,18 +79,18 @@ function displayResults(json) {
       link.textContent = current.headline.main;
       para1.textContent = current.snippet;
       para2.textContent = 'Keywords: ';
-      for (let j = 0; j < current.keywords.length; j++) {
+      for(let j = 0; j < current.keywords.length; j++) {
         const span = document.createElement('span');
         span.textContent += current.keywords[j].value + ' ';
         para2.appendChild(span);
       }
 
-      if (current.multimedia.length > 0) {
-        img.src = `https://www.nytimes.com/${current.multimedia[0].url}`;
+      if(current.multimedia.length > 0) {
+        img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
         img.alt = current.headline.main;
       }
 
-      clearfix.setAttributes('class', 'clearfix');
+      clearfix.setAttribute('class','clearfix');
 
       article.appendChild(heading);
       heading.appendChild(link);
@@ -103,11 +98,29 @@ function displayResults(json) {
       article.appendChild(para1);
       article.appendChild(para2);
       article.appendChild(clearfix);
-      article.appendChild(article);
+      section.appendChild(article);
     }
   }
 }
 
 
 
+function nextPage(e) {
+  pageNumber++;
+  fetchResults(e);
+}
+
+function previousPage(e) {
+  if (pageNumber > 0) {
+    pageNumber--;
+  } else {
+    return;
+  }
+  fetchResults(e);
+}
+
+
+
 searchForm.addEventListener('submit', submitSearch);
+nextBtn.addEventListener('click', nextPage);
+previousBtn.addEventListener('click', previousPage);
